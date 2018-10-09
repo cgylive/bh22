950a88edcaeb28d9ff0910a698c532a5506a5488
cgylive <you@example.com>
2018-10-09 16:41:46 +0800

    商品首页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+import index from './components/index.vue';
+
+Vue.use(VueRouter);
+
+
+const routes = [{
+        path: '/',
+        component: index
+    },
+    {
+        path: '/index',
+        component: index
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App),
+    router
 }).$mount('#app')
